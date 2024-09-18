c37fde416d7113305373d60e6357a363b12b493c
mhurhangee <m.hurhangee@me.com>
2024-09-18 20:40:05 +0100

    fix: fetchFromAPI.ts

--- lib/fetchFromAPI.ts
+++ lib/fetchFromAPI.ts
@@ -1,12 +1,8 @@
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-interface FetchOptions extends RequestInit {
-  // Add any additional custom options if needed
-}
-
 export const fetchFromAPI = async <T>(
   endpoint: string,
-  options: FetchOptions = {}
+  options: RequestInit = {}
 ): Promise<T> => {
   try {
     const response = await fetch(`${API_URL}${endpoint}`, {
